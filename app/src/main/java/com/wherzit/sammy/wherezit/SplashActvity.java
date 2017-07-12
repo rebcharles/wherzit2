5357c79549401795c0cec85d35d83601ebc3605a
Sammy Tbeile <st2918@columbia.edu>
2017-07-12 16:30:54 -0400

    Added SplashScreen Logic

---
+++ app/src/main/java/com/wherzit/sammy/wherezit/SplashActvity.java
@@ -0,0 +1,30 @@
+package com.wherzit.sammy.wherezit;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActvity extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed( new Runnable() {
+
+            @Override
+            public void run(){
+                Intent i = new Intent(SplashActvity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+
+            }
+        }, SPLASH_TIMER);
+
+    }
+}
