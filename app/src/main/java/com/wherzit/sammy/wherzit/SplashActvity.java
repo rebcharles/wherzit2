2ebc898287e8ebccf499262dffc88d404c7d4d40
Sammy Tbeile <st2918@columbia.edu>
2017-07-12 17:56:19 -0400

    Drop down animation

--- app/src/main/java/com/wherzit/sammy/wherzit/SplashActvity.java
+++ app/src/main/java/com/wherzit/sammy/wherzit/SplashActvity.java
@@ -2,8 +2,11 @@ package com.wherzit.sammy.wherzit;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SplashActvity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class SplashActvity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.drop_down);
+        findViewById(R.id.splashText).startAnimation(anim);
 
         new Handler().postDelayed( new Runnable() {
 
@@ -20,6 +25,7 @@ public class SplashActvity extends AppCompatActivity {
             public void run(){
                 Intent i = new Intent(SplashActvity.this, MainActivity.class);
                 startActivity(i);
+
 
                 finish();
 
