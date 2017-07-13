cfebd82cf110a938ca3217ed106af4c827642780
Sammy Tbeile <st2918@columbia.edu>
2017-07-13 17:58:11 -0400

    Scaffolding for Places API

--- app/src/main/java/com/wherzit/sammy/wherzit/MainActivity.java
+++ app/src/main/java/com/wherzit/sammy/wherzit/MainActivity.java
@@ -1,19 +1,41 @@
 package com.wherzit.sammy.wherzit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
+
 
 /**
  * Created by sammy on 7/13/17.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if(!connectionResult.isSuccess()) { //success
+            Toast.makeText(this, "Unable to connect to Google", Toast.LENGTH_SHORT).show();
+        }
     }
 }
